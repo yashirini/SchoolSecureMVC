7100bbf6a956a3b7d080874e92e690cdb257d178
yashirini <yashirini@gmail.com>
2025-08-04 13:07:27 -0600

    Create AppDbContext.cs

---
+++ Backend/School.Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolSecureMVC.Core;
+
+namespace SchoolSecureMVC.Data {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
